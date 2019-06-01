fadfcf89e18e4fce52f8702d1010dd7a7b82fb33
codedigger <>
2019-06-01 11:37:12 +0900

    no message

--- word.py
+++ word.py
@@ -12,3 +12,9 @@ class Word(object):
 
     def __eq__(self, obj):
         return isinstance(obj, Word) and obj.word == self.word
+
+    def __repr__(self):
+        return f"C({self.word})"
+
+    def __hash__(self):
+        return hash(self.word)
